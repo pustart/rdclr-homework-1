61790aea53af115f2e9c78f72b7578e97487db8b
pustart <polaris9955@gmail.com>
2023-08-02 18:11:19 +0300

    feat: add fonts

--- gulpfile.js
+++ gulpfile.js
@@ -26,136 +26,146 @@ const sass = gulpSass(dartSass);
 const projectFolder = 'prod';
 const sourceFolder = 'src';
 const path = {
-    build: {
-        html: projectFolder + '/',
-        css: projectFolder + '/styles/',
-        img: projectFolder + '/public/',
-        fonts: projectFolder + '/fonts/',
-    },
+  build: {
+    html: projectFolder + '/',
+    css: projectFolder + '/styles/',
+    img: projectFolder + '/public/',
+    fonts: projectFolder + '/fonts/',
+  },
 
-    src: {
-        html: sourceFolder + '/*.html',
-        pug: sourceFolder + '/*.pug',
-        css: sourceFolder + '/styles/*.{scss,css}',
-        img: sourceFolder + '/public/**/*.{jpg,jpeg,png,gif,ico,webp,svg}',
-        fonts: sourceFolder + '/fonts/*.{ttf,eot,otf,ttc,woff,woff2}',
-    },
+  src: {
+    html: sourceFolder + '/*.html',
+    pug: sourceFolder + '/*.pug',
+    css: sourceFolder + '/styles/*.{scss,css}',
+    img: sourceFolder + '/public/**/*.{jpg,jpeg,png,gif,ico,webp,svg}',
+    fonts: sourceFolder + '/fonts/*.{ttf,eot,otf,ttc,woff,woff2}',
+  },
 
-    watch: {
-        html: sourceFolder + '/**/*.html',
-        pug: sourceFolder + '/**/*.pug',
-        css: sourceFolder + '/styles/**/*.{scss,css}',
-        img: sourceFolder + '/public/**/*.{jpg,jpeg,png,gif,ico,webp,svg}',
-    },
+  watch: {
+    html: sourceFolder + '/**/*.html',
+    pug: sourceFolder + '/**/*.pug',
+    css: sourceFolder + '/styles/**/*.{scss,css}',
+    img: sourceFolder + '/public/**/*.{jpg,jpeg,png,gif,ico,webp,svg}',
+  },
 
-    clean: './' + projectFolder + '/'
+  clean: './' + projectFolder + '/**/*'
 };
 
 export const browserSync = () => {
-    sync.init({
-        ui: false,
-        notify: false,
-        browser: 'chrome',
-        server: {
-            baseDir: './' + projectFolder + '/'
-        },
-    });
+  sync.init({
+    ui: false,
+    notify: false,
+    browser: 'chrome',
+    server: {
+      baseDir: './' + projectFolder + '/'
+    },
+  });
 };
 
 export const html = () => {
-    return gulp.src(path.src.pug)
-        .pipe(plumber())
-        .pipe(pug({
-            verbose: true,
-        }))
-        .pipe(gulp.dest(path.build.html))
-        .pipe(gulp.src(path.src.html))
-        .pipe(plumber())
-        .pipe(fileInclude())
-        .pipe(gulpIf(isBuildFlag, removeHtml()))
-        .pipe(gulpIf(isBuildFlag, htmlMin({
-            removeComments: true,
-        })))
-        .pipe(gulp.dest(path.build.html))
-        .pipe(sync.stream());
+  return gulp.src(path.src.pug)
+    .pipe(plumber())
+    .pipe(pug({
+      verbose: true,
+    }))
+    .pipe(gulp.dest(path.build.html))
+    .pipe(gulp.src(path.src.html))
+    .pipe(plumber())
+    .pipe(fileInclude())
+    .pipe(gulpIf(isBuildFlag, removeHtml()))
+    .pipe(gulpIf(isBuildFlag, htmlMin({
+      removeComments: true,
+    })))
+    .pipe(gulp.dest(path.build.html))
+    .pipe(sync.stream());
 };
 
 export const css = () => {
-    return gulp.src(path.src.css, { sourcemaps: !isBuildFlag })
-        .pipe(plumber())
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }))
-        .pipe(postCss([
-            autoprefixer,
-            minmax,
-        ]))
-        .pipe(groupMedia())
-        .pipe(gulp.dest(path.build.css, { sourcemaps: !isBuildFlag }))
-        .pipe(sync.stream());
+  return gulp.src(path.src.css, { sourcemaps: !isBuildFlag })
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(postCss([
+      autoprefixer,
+      minmax,
+    ]))
+    .pipe(groupMedia())
+    .pipe(gulp.dest(path.build.css, { sourcemaps: !isBuildFlag }))
+    .pipe(sync.stream());
 };
 
 export const images = () => {
-    return gulp.src(path.src.img)
-        .pipe(plumber())
-        .pipe(webp())
-        .pipe(gulp.dest(path.build.img))
-        .pipe(gulp.src(path.src.img))
-        .pipe(gulpIf(isBuildFlag, imagemin({
-            progressive: true,
-            verbose: true,
-            optimizationLevel: 3
-        })))
-        .pipe(gulp.dest(path.build.img));
+  return gulp.src(path.src.img)
+    .pipe(plumber())
+    .pipe(webp())
+    .pipe(gulp.dest(path.build.img))
+    .pipe(gulp.src(path.src.img))
+    .pipe(gulpIf(isBuildFlag, imagemin({
+      progressive: true,
+      verbose: true,
+      optimizationLevel: 3
+    })))
+    .pipe(gulp.dest(path.build.img));
 };
 
-export const fonts = () => {
-    return gulp.src(path.src.fonts)
-        .pipe(plumber())
-        .pipe(fonter({
-            formats: ['ttf', 'eot', 'woff', 'svg']
-        }))
-        .pipe(gulp.dest(path.build.fonts))
-        .pipe(ttf2woff2())
-        .pipe(gulp.dest(path.build.fonts));
+const fonts = () => {
+  return gulp.src(path.src.fonts)
+    .pipe(plumber())
+    .pipe(fonter({
+      formats: ['ttf', 'eot', 'woff', 'svg']
+    }))
+    .pipe(gulp.dest(path.build.fonts))
+    .pipe(ttf2woff2())
+    .pipe(gulp.dest(path.build.fonts));
 };
 
 export const clean = () => {
+  if (isBuildFlag) {
     return del(path.clean);
+  }
+
+  return del([path.clean, `!./${projectFolder}/fonts/**`]);
+};
+
+export const cleanFonts = () => {
+  return del(path.build.fonts);
 };
 
 const setMode = (isBuild) => {
-    return cb => {
-        isBuildFlag = isBuild;
-        cb();
-    };
+  return cb => {
+    isBuildFlag = isBuild;
+    cb();
+  };
 };
 
 export const watchFiles = () => {
-    gulp.watch([path.watch.html], html);
-    gulp.watch([path.watch.pug], pug);
-    gulp.watch([path.watch.css], css);
-    gulp.watch([path.watch.img], images);
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.pug], pug);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.img], images);
 };
 
 const watch = gulp.parallel(
-    watchFiles,
-    browserSync);
+  watchFiles,
+  browserSync);
 
 const dev = gulp.parallel(
-    css,
-    html,
-    images);
+  css,
+  html,
+  images);
 
 export const build = gulp.series(
-    clean,
-    setMode(true),
-    dev,
-    fonts,
-    browserSync);
+  clean,
+  setMode(true),
+  dev,
+  fonts,
+  browserSync);
 
 export default gulp.series(
-    clean,
-    dev,
-    watch,
+  clean,
+  dev,
+  watch,
 );
+
+export const fnts = gulp.series(cleanFonts, fonts);
